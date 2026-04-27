a6694805d37aaf743342442a639ed2ccf8c29d5c
harmel3056 <melh1988@hotmail.com>
2026-04-27 12:52:59 +1000

    Syntax amendment

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -48,10 +48,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
     /* delete middle or tail node */
     tmp = *head;
 
-    if (tmp->prev)
+    if (tmp->prev != NULL)
         tmp->prev->next = tmp->next;
 
-    if (tmp->next)
+    if (tmp->next != NULL)
         tmp->next->prev = tmp->prev;
 
     free(tmp);
