8757ea4d9038b93ccd8c58f5a1419d7d960bbba9
harmel3056 <melh1988@hotmail.com>
2026-04-27 12:48:13 +1000

    Fix My Code challenges

---
+++ README.md
@@ -0,0 +1,9 @@
+# **Background Context**
+
+`Fix my code` is a new type of project, where we'll jump into an existing code base and fix it!
+
+Sometimes you will know the language, sometimes not.
+
+Please download the repository [Fix_My_Code_Challenge](https://intranet.hbtn.io/rltoken/vj9owR_ZFWviI1EWWp_wKg) and use it as initial files for all solutions.
+
+You should not recode everything, just fix it!
---
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *saved_head, *tmp;
+    unsigned int p = 0;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    saved_head = *head;
+
+    /* move to the node at index */
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+
+    if (*head == NULL)  /* index out of range */
+    {
+        *head = saved_head;
+        return (-1);
+    }
+
+    /* delete head */
+    if (index == 0)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+
+        if (tmp)
+            tmp->prev = NULL;
+
+        return (1);
+    }
+
+    /* delete middle or tail node */
+    tmp = *head;
+
+    if (tmp->prev)
+        tmp->prev->next = tmp->next;
+
+    if (tmp->next)
+        tmp->next->prev = tmp->prev;
+
+    free(tmp);
+
+    *head = saved_head;
+    return (1);
+}
